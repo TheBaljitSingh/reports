8c61b54046e8ac75cee386a6b4566fc6077cf208
baljitsingh-alt <baljit@altrodav.com>
2025-09-08 16:20:48 +0530

    updated

--- server/queue/redis.js
+++ server/queue/redis.js
@@ -3,14 +3,14 @@ import { configDotenv } from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+if (process.env.NODE_ENV !== "production") {
+  // Only load .env locally
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  configDotenv({ path: path.resolve(__dirname, "../.env") });
+}
+// env is not wokring currently
 
-// Load .env from project root explicitly, regardless of cwd
-configDotenv({ path: path.resolve(__dirname, "../.env") });
-
-
-console.log("printing",process.env.REDIS_ENDPOINT);
 export const redisClient= ()=> {
 
     return new IORedis(process.env.REDIS_URL);
