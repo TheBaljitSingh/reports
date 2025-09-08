0f2e9bd15ee4e4c25711486b776fae5185bc88f6
baljitsingh-alt <baljit@altrodav.com>
2025-09-08 19:30:15 +0530

    update

--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -1,8 +1,9 @@
 import multer from "multer";
+import os from "os";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './uploads')
+    cb(null, os.tmpdir())
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
